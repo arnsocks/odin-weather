0ca179523017a5471251c555cc1b951d89c3c192
James Arn <132308884+arnsocks@users.noreply.github.com>
2025-07-08 11:06:50 -0400

    Set up initial files

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let heading = document.querySelector(".heading");
+
+heading.style.backgroundColor = "blue";
+
+const getWeather = async () => {
+  console.log("go get the weather");
+};
+
+getWeather();
