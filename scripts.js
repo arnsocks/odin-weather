00030e67b8262b7565e5eb1fca11ae9556b4df15
James Arn <132308884+arnsocks@users.noreply.github.com>
2025-07-21 11:15:20 -0400

    Get back end working with user input location.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,40 @@
 let heading = document.querySelector(".heading");
+const button = document.querySelector("button");
 
 heading.style.backgroundColor = "blue";
 
-const getWeather = async () => {
-  console.log("go get the weather");
+const getWeather = async (city, unit = "us") => {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=${unit}&include=current&key=CGFGTC498RSULEKK8AJRBS8N2&contentType=json`
+    );
+    if (!response.ok) throw new Error(response.status); // make sure the response is OK
+    const weatherData = await response.json();
+    console.log(weatherData);
+    return weatherData;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-getWeather();
+const processData = (weatherJSON) => {
+  let weatherData = {};
+
+  weatherData.currentConditions = weatherJSON.currentConditions;
+  weatherData.days = {};
+  for (let i = 0; i < 7; i++) {
+    weatherData.days[i] = weatherJSON.days[i];
+  }
+
+  return weatherData;
+};
+
+button.addEventListener("click", () => {
+  const city = prompt("What city?");
+  getWeather(city)
+    .then(processData)
+    .then((weatherData) => {
+      heading.textContent = `It's ${weatherData.currentConditions.temp}° F right now in ${weatherData.resolvedAddress}.`;
+      return weatherData;
+    });
+});
